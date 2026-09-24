66a8ccce0916af75a8c6eba5588157f8dc97b624
capitanshop1137 <www.amircapitan18@gmail.com>
2026-09-24 18:37:50 +0330

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// ===== تنظیمات Supabase =====
+const SUPABASE_URL = 'https://kkkzshtryrarbtsfwmpf.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_-2sKuRllydWTz13Ad6VIgA_z_pnw4ya';
+const BUCKET_NAME = 'account-images';
